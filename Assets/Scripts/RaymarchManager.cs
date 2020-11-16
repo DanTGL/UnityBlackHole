1815df63981c0ab0a0b0f729ed98420b327c030f
DanTGL <33489389+DanTGL@users.noreply.github.com>
2020-11-16 12:54:11 +0100

    Initial commit

---
+++ Assets/Scripts/RaymarchManager.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode, ImageEffectAllowedInSceneView]
+public class RaymarchManager : MonoBehaviour {
+    
+    public ComputeShader computeShader;
+
+    private RenderTexture target;
+
+    private Camera cam;
+
+    private int raymarchKernel;
+
+    struct ObjectData {
+        public int shape;
+        public Vector3 position;
+        public Vector3 size;
+        public Vector3 rotation;
+        public Color surfaceColor;
+    }
+
+    private List<ObjectData> objects;
+
+    void Start() {
+        cam = GetComponent<Camera>();
+        if (computeShader != null) {
+            raymarchKernel = computeShader.FindKernel("CSMain");
+        }
+
+        FindObjects();
+    }
+
+    void FindObjects() {
+        objects = new List<ObjectData>();
+        RaymarchShape[] foundObjs = FindObjectsOfType<RaymarchShape>();
+        for (int i = 0; i < foundObjs.Length; i++) {
+            ObjectData obj;
+            obj.shape = foundObjs[i].GetShape();
+            obj.position = foundObjs[i].GetPosition();
+            obj.size = foundObjs[i].GetSize();
+            obj.rotation = foundObjs[i].GetRotation();
+            obj.surfaceColor = foundObjs[i].GetSurfaceColor();
+            objects.Add(obj);
+        }
+        
+        
+    }
+
+    [ImageEffectOpaque]
+    void OnRenderImage(RenderTexture source, RenderTexture dest) {
+        if (computeShader == null) {
+            Graphics.Blit(source, dest);
+            return;
+        }
+        
+        InitRenderTexture();
+
+        FindObjects();
+
+        ComputeBuffer buffer = new ComputeBuffer(objects.Count, 56);
+        buffer.SetData(objects.ToArray());
+
+        computeShader.SetMatrix("_CameraToWorld", cam.cameraToWorldMatrix);
+        computeShader.SetMatrix("_CameraInverseProjection", cam.projectionMatrix.inverse);
+        computeShader.SetInt("_numShapes", objects.Count);
+        computeShader.SetBuffer(raymarchKernel, "dataBuffer", buffer);
+        //computeShader.SetTexture(raymarchKernel, "_SkyboxTexture", RenderSettings.skybox.mainTexture);
+        computeShader.SetTexture(raymarchKernel, "Result", target);
+        computeShader.Dispatch(raymarchKernel, cam.pixelWidth / 8, cam.pixelHeight / 8, 1);
+        buffer.Release();
+
+        Graphics.Blit(target, dest);
+    }
+
+    void InitRenderTexture() {
+        if (target == null || target.width != Screen.width || target.height != Screen.height) {
+
+            if (target != null) {
+                target.Release();
+            }
+
+            target = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            target.enableRandomWrite = true;
+            target.Create();
+        }
+    }
+
+}
---
+++ Assets/Scripts/RaymarchShape.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaymarchShape : MonoBehaviour {
+
+    enum ShapeType {
+        NONE,
+        SPHERE,
+        CUBE,
+        CYLINDER
+    }
+
+    [SerializeField]
+    private ShapeType shape = ShapeType.NONE;
+
+    [SerializeField]
+    private Vector3 size = Vector3.one;
+
+    [SerializeField]
+    private Color surfaceColor = Color.white;
+
+    public Vector3 GetPosition() {
+        return transform.position;
+    }
+
+    public int GetShape() {
+        return (int) shape;
+    }
+
+    public Color GetSurfaceColor() {
+        return surfaceColor;
+    }
+
+    public Vector3 GetSize() {
+        return Vector3.Scale(size, transform.localScale);
+    }
+
+    public Vector3 GetRotation() {
+        return transform.rotation.eulerAngles * Mathf.Deg2Rad;
+    }
+
+}
